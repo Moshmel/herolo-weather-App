31bd24892a2b2084d2496b56eb20fbe68059c681
Moshe Melamed <melamed2@gmail.com>
2019-07-27 16:51:29 +0300

    first commit2222

---
+++ precache-manifest.a7b37bcca33034200e648dfffbac5943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ec8a8811488b3447eeace843455844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51214dba76ef18da9a9d",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "0e6581772457c40c2eba",
+    "url": "/static/css/main.9f46cbdc.chunk.css"
+  },
+  {
+    "revision": "51214dba76ef18da9a9d",
+    "url": "/static/js/2.2c2006b7.chunk.js"
+  },
+  {
+    "revision": "0e6581772457c40c2eba",
+    "url": "/static/js/main.6072c96f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
